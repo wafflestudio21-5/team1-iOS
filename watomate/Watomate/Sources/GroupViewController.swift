749456191c403501fdef73b32407378d54d18474
cirtuare <cirtuare@gmail.com>
2023-12-31 14:54:24 +0900

    feat:tabBarController

---
+++ watomate/Watomate/Sources/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  Watomate
+//
+//  Created by 이수민 on 2023/12/31.
+//  Copyright © 2023 tuist.io. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ watomate/Watomate/Sources/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Watomate
+//
+//  Created by 이수민 on 2023/12/31.
+//  Copyright © 2023 tuist.io. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+        
+        let todoVC = UINavigationController(rootViewController: ToDoViewController())
+        todoVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
+
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        
+        let groupVC = UINavigationController(rootViewController: GroupViewController())
+        groupVC.tabBarItem = UITabBarItem(title: "Group", image: UIImage(systemName: "rectangle.3.group.fill"), tag: 2)
+
+        let profileVC = UINavigationController(rootViewController: ProfileViewController())
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle.fill"), tag: 3)
+        
+        viewControllers = [todoVC, searchVC, groupVC, profileVC]
+    }
+}
