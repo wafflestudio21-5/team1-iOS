53f10100961141d71c04486d0a06879d9fd5d0b7
JH713 <strider13@snu.ac.kr>
2024-01-31 15:58:33 +0900

    feat: tabbar 수정

--- watomate/Watomate/Sources/TabBarController.swift
+++ watomate/Watomate/Sources/TabBarController.swift
@@ -12,23 +12,29 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemBackground
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        tabBar.layer.borderWidth = 0.5
+        tabBar.layer.borderColor = UIColor.clear.cgColor
+        tabBar.clipsToBounds = true 
         
         let todoVC = UINavigationController(rootViewController: ToDoViewController())
-        todoVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
+        todoVC.tabBarItem = UITabBarItem(title: "피드", image: UIImage(named: "home"), tag: 0)
 
         let searchVC = UINavigationController(rootViewController: SearchViewController())
-        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        searchVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(named: "search"), tag: 1)
         
 
         let groupVC = UINavigationController(rootViewController: GroupViewController())
-        groupVC.tabBarItem = UITabBarItem(title: "Group", image: UIImage(systemName: "rectangle.3.group.fill"), tag: 2)
+        groupVC.tabBarItem = UITabBarItem(title: "로그아웃", image: UIImage(systemName: "xmark"), tag: 2)
 
         let todoRepository = TodoRepository()
         let todoUseCase = TodoUseCase(todoRepository: todoRepository)
         let todoListViewModel = TodoListViewModel(todoUseCase: todoUseCase)
         let profileVC = UINavigationController(rootViewController: ProfileViewController(todoListViewModel: todoListViewModel))
 
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle.fill"), tag: 3)
+        profileVC.tabBarItem = UITabBarItem(title: "My", image: UIImage(named: "my"), tag: 3)
 
         viewControllers = [todoVC, searchVC, groupVC, profileVC]
     }
