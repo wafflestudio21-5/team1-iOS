a54d2ddedfbab51d3acc24ca56c5bd4f1196fd8a
h9kwon <harryk0702@gmail.com>
2024-01-31 02:40:26 -0500

    feat: merge tableView, construct basic ui for mainpage without following user info

---
+++ watomate/Watomate/Sources/CalendarHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarHeaderView.swift
+//  Watomate
+//
+//  Created by 권현구 on 1/30/24.
+//  Copyright © 2024 tuist.io. All rights reserved.
+//
+
+import UIKit
+
+class CalendarHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
